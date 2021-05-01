355e33cc9942b776f25d57d9ee287967d79963d9
Pavel <pavelaz89@gmail.com>
2021-05-01 23:07:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/* let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log (incr);
+console.log(decr); */
+
+/* let incr = 10,
+    decr = 10;
+++incr;
+--decr;
+console.log (incr); //результат 11
+console.log(decr); //результат 9 */
+
+/* let incr = 10,
+    decr = 10;
+incr;
+decr;
+console.log (incr++); //результат 10
+console.log(decr--); //результат 10 */
+
+let incr = 10,
+    decr = 10;
+incr;
+decr;
+console.log (++incr); //результат 11
+console.log(--decr); // результат 9
